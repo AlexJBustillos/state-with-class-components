467801608b8f73c7ddbbea10d9f7dc670c0d2ad1
alexjbustillos <alexjbustillos@gmail.com>
2020-12-09 10:33:19 -0500

    [count]w/ add button and class component w/ setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+ 
+class App extends Component {
+    constructor() {
+        super()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        this.state = {
+            count: 0,
+        }
+    }
+    increment = () => {
+        const currentCount = this.state.count
+        const newCount = currentCount + 1
+
+        this.setState({ count: newCount })
+    }
+
+    render() {
+        return(
+            <div>
+                <h1> {this.state.count} </h1>
+                <button onClick={this.increment}>+</button>
+            </div>
+        )
+    }
 }
-
 export default App;
