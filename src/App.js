7b55182a102ec51239ebc4b31b66741588959bb7
alexjbustillos <alexjbustillos@gmail.com>
2020-12-09 10:49:01 -0500

    [decrement]added w/minus button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { Component } from 'react';
  
@@ -16,12 +16,16 @@ class App extends Component {
 
         this.setState({ count: newCount })
     }
+    decrement = () => {
+        this.setState({ count: this.state.count - 1 })
+    }
 
     render() {
         return(
             <div>
                 <h1> {this.state.count} </h1>
                 <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
             </div>
         )
     }
